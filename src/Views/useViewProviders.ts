044b8f871774142b0bdf34fbb54a99bf82971739
qunwang6 <qunwang6@gmail.com>
2024-12-04 12:41:18 +0800

    Update useViewProviders.ts

--- src/Views/useViewProviders.ts
+++ src/Views/useViewProviders.ts
@@ -3,7 +3,7 @@ import type { ViewProvider } from "@/viewProvider";
 export const useTimelineExternalProvider = () => ({
   id: "markwhen.timeline",
   name: "Timeline",
-  url: "https://timeline.markwhen.com",
+  url: "http://localhost:5173",
   iconSvg: `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" class="h-full w-full"><path 
   fill="currentColor" 
   d="m 13 13 h -5 c -0.55 0 -1 -0.45 -1 -1 s 0.45 -1 1 -1 h 5 c 0.55 0 1 0.45 1 1 s -0.45 1 -1 1 z m -1 -4 h -7 c -0.55 0 -1 -0.45 -1 -1 s 0.45 -1 1 -1 h 7 c 0.55 0 1 0.45 1 1 s -0.45 1 -1 1 z m -9 -4 h 0 c -0.55 0 -1 -0.45 -1 -1 s 0.45 -1 1 -1 h 2 c 0.55 0 1 0.45 1 1 s -0.45 1 -1 1 z">
